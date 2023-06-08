7b452afa3ea9d0d9e1092980e6cae376520de638
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2023-06-09 01:07:24 +0530

    LC CPP

--- Leetcode (CPP)/1351.count_negative_numbers_in_a_sorted_matrix.cpp
+++ Leetcode (CPP)/1351.count_negative_numbers_in_a_sorted_matrix.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) { //T.C.=O(m+n)
-        int m=grid.size(), n=grid[0].size(), count=0;
-        int row=0, col=n-1; //start at first row and last col
-        while(row<m && col>=0)
-        {
-            if(grid[row][col]<0)
-            {
-                count+=(m-row);
-                col--;
-            }
-            else
-                row++;
-        }
+    int countNegatives(vector<vector<int>>& grid) { // T.C.=O(m*n)
+        int m=grid.size(), n=grid[0].size();
+        int count = 0;
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                if(grid[i][j] < 0)
+                    count++;
         return count;
     }
 };
