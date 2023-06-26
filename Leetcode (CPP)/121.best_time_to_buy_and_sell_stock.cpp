b1745c03b5cdf9e2bb80018e4069a1e318a0060f
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2023-06-27 00:34:34 +0530

    LC CPP

--- Leetcode (CPP)/121.best_time_to_buy_and_sell_stock.cpp
+++ Leetcode (CPP)/121.best_time_to_buy_and_sell_stock.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int min_price=prices[0],max_profit=0;
-        for(int i=0; i<n; i++)
+    int maxProfit(vector<int>& prices) { // T.C.=O(n)
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int price: prices)
         {
-            if(prices[i] < min_price)
-                min_price=prices[i];
-            else if(prices[i]-min_price > max_profit)
-                max_profit=prices[i]-min_price;
+            // new min price found
+            if(price < minPrice) 
+                minPrice = price;
+            // new max profit found
+            else if(price - minPrice > maxProfit)
+                maxProfit = price - minPrice;
         }
-        return max_profit;
+        return maxProfit;
     }
 };
-
-/*
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size(),max=0,profit;
-        for(int i=0; i<n-1; i++)
-        {
-            for(int j=i+1; j<n; j++)
-            {
-                profit=prices[j]-prices[i];
-                if(profit > max)
-                    max=profit;
-            }
-        }
-        return max;
-    }
-};
-*/
--- Leetcode (CPP)/122.best_time_to_buy_and_sell_stock_II.cpp
+++ Leetcode (CPP)/122.best_time_to_buy_and_sell_stock_II.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int diff,sum=0;
-        for(int i=1; i<n; i++)
-        {
-            diff=prices[i]-prices[i-1];
-            if(diff > 0)
-                sum=sum+diff;
-        }
+        int sum = 0;
+        for(int i=1; i<prices.size(); i++)
+            if(prices[i] - prices[i-1] > 0)
+                sum += prices[i] - prices[i-1];
         return sum;
     }
 };
