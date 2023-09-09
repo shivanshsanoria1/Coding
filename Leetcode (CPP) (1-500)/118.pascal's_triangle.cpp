64a1f661556cd9671ca2457d6fd83ecb84db15ea
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2023-09-09 22:39:14 +0530

    LC CPP

--- Leetcode (CPP) (1-500)/118.pascal's_triangle.cpp
+++ Leetcode (CPP) (1-500)/118.pascal's_triangle.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result(numRows);
+        vector<vector<int>> ans(numRows);
         for(int i=0; i<numRows; i++)
         {
-            result[i].resize(i+1); // give size of column
-            result[i][0]=1; // first element of each row is 1
-            result[i][i]=1; // last element of each row is 1
-            for(int j=1; j<=i-1; j++)
-                result[i][j]=result[i-1][j]+result[i-1][j-1];
+            ans[i].resize(i+1); // give size of column
+            ans[i][0] = 1; // first element of each row is 1
+            ans[i][i] = 1; // last element of each row is 1
+            for(int j=1; j<i; j++)
+                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
         }
-        return result;
+        return ans;
     }
 };
